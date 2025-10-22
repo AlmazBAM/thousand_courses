424cd21dac410ea246b958c40f286e3eb39c2d4d
AlmazBag <almazbagmanov@yandex.ru>
2025-10-22 14:51:33 +0300

    Изменена регулярка для проверки корректности почты

--- app/src/main/java/com/bagmanovam/thousand_courses/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/bagmanovam/thousand_courses/presentation/login/LoginViewModel.kt
@@ -5,11 +5,12 @@ import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.derivedStateOf
 import androidx.lifecycle.ViewModel
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel : ViewModel() {
 
     val emailState = TextFieldState()
     val isValid = derivedStateOf {
-        Patterns.EMAIL_ADDRESS.matcher(emailState.text.toString()).matches()
+        val regex = "^[A-Za-z0-9+-_.]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$".toRegex()
+        emailState.text.toString().matches(regex)
     }
     val passwordState = TextFieldState()
 }
